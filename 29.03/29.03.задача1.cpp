8d8f5ca2e13cccd156a8e65ad22c7317aaaaf861
kupuzz <90672628+kupuzz@users.noreply.github.com>
2022-03-29 11:53:28 +0300

    Add files via upload

---
+++ 29.03/29.03.задача1.cpp
@@ -0,0 +1,90 @@
+﻿// 29.03.задача1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+
+struct STUDENT
+{
+    std::string NAME;
+    std::string FIRSTNAME;
+    int GROUP = 0;
+    int SES[5] = { 0,0,0,0,0 };
+};
+
+void enter(STUDENT* STUD1, int h)
+{
+    for (int i = 0; i < h; i++)
+    {
+        std::cin >> STUD1[i].NAME;
+        std::cin >> STUD1[i].FIRSTNAME;
+        std::cin >> STUD1[i].GROUP;
+        for (int k = 0; k < 5; k++)
+        {
+            std::cin >> STUD1[i].SES[k];
+        }
+    }
+    return;
+}
+
+
+
+void sort(STUDENT* STUD1, int h, int *a)
+{
+    int p = 0;
+    for (int i = 0; i < h - 1; i++)
+    {
+        if (STUD1[a[i]].NAME.compare(STUD1[a[i + 1]].NAME) == 1)
+        {
+            int k = a[i];
+            a[i] = a[i + 1];
+            a[i + 1] = k;
+            p = 1;
+        }
+    }
+    if (p == 1)
+    {
+        sort(STUD1, h, a);
+    }
+    if (p == 0)
+    {
+        return;
+    }
+}
+
+void work(STUDENT* STUD1, int h, int *a)
+{
+    int p = 0;
+    for (int i = 0; i < h; i++)
+    {
+        for (int k = 0; k < 5; k++)
+        {
+            if (STUD1[a[i]].SES[k] == 2)
+            {
+                std::cout << STUD1[a[i]].NAME + ' ' + STUD1[a[i]].FIRSTNAME << ' ' << STUD1[a[i]].GROUP << '\n';
+                p = 1;
+                break;
+            }
+        }
+    }
+    if (p == 0)
+    {
+        std::cout << '0';
+    }
+    return;
+}
+
+int main()
+{
+    const int h = 10;
+    STUDENT STUD1[h];
+    int a[h];
+    for (int i = 0; i < h; i++)
+    {
+        a[i] = i;
+    }
+    enter(STUD1, h);
+    sort(STUD1, h, a);
+    work(STUD1, h, a);
+}
+
---
+++ 29.03/29.03.задача2.cpp
@@ -0,0 +1,93 @@
+﻿// 29.03.задача2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+
+struct STUDENT
+{
+    std::string NAME;
+    std::string SECONDNAME;
+    int AGE = 0;
+    int CLASS = 0;
+    int H = 0;
+    int MARKS[30] = {0};
+};
+
+void enter(STUDENT* S, int h)
+{
+    for (int i = 0; i < h; i++)
+    {
+        std::cin >> S[i].NAME;
+        std::cin >> S[i].SECONDNAME;
+        std::cin >> S[i].AGE;
+        std::cin >> S[i].CLASS;
+        std::cin >> S[i].H;
+        for (int k = 0; k < S[i].H; k++)
+        {
+            std::cin >> S[i].MARKS[k];
+        }
+    }
+    return;
+}
+
+void add_mark(STUDENT& s, int mark)
+{
+    s.H += 1;
+    s.MARKS[s.H - 1] = mark;
+    return;
+}
+
+void work(STUDENT* S, int h)
+{
+    int p = 0;
+    for (int i = 0; i < h; i++)
+    {
+        float sum = 0;
+        for (int k = 0; k < S[i].H; k++)
+        {
+            sum += S[i].MARKS[k];
+        }
+        float mid = sum / S[i].H;
+        if (mid < 4.5)
+        {
+            add_mark(S[i], 5);
+            p = 1;
+        }
+    }
+    if (p == 1)
+    {
+        work(S, h);
+    }
+    if (p == 0)
+    {
+        return;
+    }
+}
+
+void output(STUDENT* S, int h)
+{
+    for (int i = 0; i < h; i++)
+    {
+        std::cout << S[i].NAME << " ";
+        std::cout << S[i].SECONDNAME << " ";
+        std::cout << S[i].AGE << " ";
+        std::cout << S[i].CLASS << " ";
+        std::cout << S[i].H << "\n";
+        for (int k = 0; k < S[i].H; k++)
+        {
+            std::cout << S[i].MARKS[k] << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
+int main()
+{
+    const int h = 5;
+    STUDENT S[h];
+    enter(S, h);
+    work(S, h);
+    output(S, h);
+}
+
